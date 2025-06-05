7ebaf67ca6ee9e1b32c2bb0a08ada9920f7902ee
bymudo <santiagoramonrintoul@gmail.com>
2025-06-05 17:59:43 +0200

    Update App.jsx

--- parquin-web-final/src/App.jsx
+++ parquin-web-final/src/App.jsx
@@ -137,7 +137,7 @@ export default function App() {
 
     actualizarPorClimaYHorario();
 
-    const intervalo = setInterval(actualizarPorClimaYHorario, 5 * 60 * 1000);
+    const intervalo = setInterval(actualizarPorClimaYHorario, 10000);
     return () => clearInterval(intervalo);
   }, []);
 
